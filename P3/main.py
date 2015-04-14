94f96bf73b52619d0d861ae1e0d9c49052f8ea23
Miguel Castillo Lloret <miguel.castillolloret@alum.uca.es>
2015-04-14 16:43:31 +0200

    Introducido formulario de busqueda

--- P3/main.py
+++ P3/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 from flask.ext.googlemaps import GoogleMaps
 from flask.ext.googlemaps import Map
 
@@ -8,29 +9,37 @@ import json
 app = Flask(__name__)
 GoogleMaps(app)
 
-twitter = Twitter.oauth_login()
+@app.route("/buscar", methods=['POST'])
+def mapview():
 
-tweets = twitter.search.tweets(q='Star Citizen', count=1000, geocode='40.2085,-3.713,497mi');
+	twitter = Twitter.oauth_login()
 
-Twitter.save_json('tweets', tweets);
+	formtext = request.form['text']
 
-fileload = Twitter.load_json('tweets');
+	tweets = twitter.search.tweets(q=formtext, count=1000, geocode='40.2085,-3.713,497mi');
 
-search = json.loads(fileload);
+	Twitter.save_json('tweets', tweets);
 
-results = []
+	fileload = Twitter.load_json('tweets');
 
-for i in search['statuses']:
-	if i['geo']:
-		xy=	(i['geo']['coordinates'][0], i['geo']['coordinates'][1])
-		results.append(xy)
+	search = json.loads(fileload);
 
-print(results)
+	results = []
+
+	for i in search['statuses']:
+		if i['geo']:
+			xy=	(i['geo']['coordinates'][0], i['geo']['coordinates'][1])
+			results.append(xy)
+
+	print(results)
+
+	mymap = Map(identifier="view-side", lat=40.45, lng=3.75, markers=results, style="height:700px;width:700px;margin:0 auto;", zoom=4)
+
+	return render_template('mapa.html', mymap=mymap)
 
 @app.route("/")
-def mapview():
-	mymap = Map(identifier="view-side", lat=40.45, lng=3.75, markers=results, style="height:700px;width:700px;margin:0 auto;", zoom=4)
-	return render_template('index.html', mymap=mymap)
+def indexview():
+	return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
